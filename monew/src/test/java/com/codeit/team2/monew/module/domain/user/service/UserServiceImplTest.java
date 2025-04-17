d8d35e7ade652513ed3551e696a0c602576cc549
김주언 <wndjs803@naver.com>
2025-04-17 15:25:32 +0900

    feat: registUserTest (green) #26

--- monew/src/test/java/com/codeit/team2/monew/module/domain/user/service/UserServiceImplTest.java
+++ monew/src/test/java/com/codeit/team2/monew/module/domain/user/service/UserServiceImplTest.java
@@ -1,5 +1,12 @@
 package com.codeit.team2.monew.module.domain.user.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import com.codeit.team2.monew.module.domain.user.dto.UserDto;
+import com.codeit.team2.monew.module.domain.user.dto.UserRegisterRequest;
+import com.codeit.team2.monew.module.domain.user.entity.User;
 import com.codeit.team2.monew.module.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,13 +24,20 @@ class UserServiceImplTest {
     private UserServiceImpl userService;
 
     @Test
-    void userCreate() {
+    void registUserTest() {
         // given
+        String email = "email";
+        String nickname = "nickname";
+        String password = "password";
+
         UserRegisterRequest request =
-            new UserRegisterRequest("email", "nickname", "passowrd");
+            new UserRegisterRequest(email, nickname, password);
+
+        User user = new User(email, nickname, password, false);
+        when(userRepository.save(any())).thenReturn(user);
 
         // when
-        UserDto userDto = UseruserService.createUser(request);
+        UserDto userDto = userService.registUser(request);
 
         // then
         assertEquals("email", userDto.email());
