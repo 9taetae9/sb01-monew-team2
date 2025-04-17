139334a5c80169d5c5a909e8a065a41c2dd0017a
김주언 <wndjs803@naver.com>
2025-04-17 11:29:00 +0900

    feat: Member entity 정의

---
+++ monew/src/main/java/com/codeit/team2/monew/module/domain/member/entity/Member.java
@@ -0,0 +1,29 @@
+package com.codeit.team2.monew.module.domain.member.entity;
+
+import com.codeit.team2.monew.module.domain.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+
+    private Boolean deleted;
+}
