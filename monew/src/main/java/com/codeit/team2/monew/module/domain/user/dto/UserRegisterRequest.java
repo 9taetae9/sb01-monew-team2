a1bac9b55b2c5e77a84e5f222ae2dd1927953da1
김주언 <wndjs803@naver.com>
2025-04-17 15:20:34 +0900

    fix: UserRegisterRequest 필드명 변경 #26

--- monew/src/main/java/com/codeit/team2/monew/module/domain/user/dto/UserRegisterRequest.java
+++ monew/src/main/java/com/codeit/team2/monew/module/domain/user/dto/UserRegisterRequest.java
@@ -1,8 +1,8 @@
 package com.codeit.team2.monew.module.domain.user.dto;
 
 public record UserRegisterRequest(
-    String nickname,
     String email,
+    String nickname,
     String password
 ) {
 
