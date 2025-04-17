2284e659b05acc268c41a2a629cc6e4d03e0f1b8
Kiki1875b <kevinheo0413@gmail.com>
2025-04-17 11:29:03 +0900

    feat: added test file for NaverNewsClient, added baseUrl in application.yml

---
+++ monew/src/test/java/com/codeit/team2/monew/module/domain/article/external/NaverNewsClientTest.java
@@ -0,0 +1,11 @@
+package com.codeit.team2.monew.module.domain.article.external;
+
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class NaverNewsClientTest {
+
+    
+}
