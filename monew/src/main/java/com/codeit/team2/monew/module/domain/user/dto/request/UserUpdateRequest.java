ef4099268a05315c290e0d0a670b7936cb97a3e1
김주언 <wndjs803@naver.com>
2025-04-18 13:40:15 +0900

    feat: bean validation #32

--- monew/src/main/java/com/codeit/team2/monew/module/domain/user/dto/request/UserUpdateRequest.java
+++ monew/src/main/java/com/codeit/team2/monew/module/domain/user/dto/request/UserUpdateRequest.java
@@ -1,7 +1,10 @@
 package com.codeit.team2.monew.module.domain.user.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record UserUpdateRequest(
 
+    @NotBlank
     String nickname
 ) {
 
