e5ff6509da3235a2daa2cd99de64dc46dc71e94b
Kiki1875b <kevinheo0413@gmail.com>
2025-04-20 15:51:42 +0900

    test: added test for KeywordReader, Red Cycle of TDD #43

--- monew/src/test/java/com/codeit/team2/monew/module/domain/article/batch/ArticleBatchItemReaderTest.java
+++ monew/src/test/java/com/codeit/team2/monew/module/domain/article/batch/ArticleBatchItemReaderTest.java
@@ -1,17 +1,30 @@
 package com.codeit.team2.monew.module.domain.article.batch;
 
 // TODO : Keyword repository 완료시 작업
-//
-//@JdbcTest
-//@Import({KeywordReaderConfig.class})
-//public class ArticleBatchItemReaderTest {
-//
-//
-//    @Autowired
-//    private JdbcPagingItemReader<Keyword> keywordReader;
-//
-//    @Test
-//    void keywordReader_shouldReturn_keywords() {
-//
-//    }
-//}
+
+import com.codeit.team2.monew.module.domain.interest.entity.Keyword;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+
+@JdbcTest
+@Import({KeywordReaderConfig.class})
+public class ArticleBatchItemReaderTest {
+
+
+    @Autowired
+    private JdbcPagingItemReader<Keyword> keywordReader;
+
+    @Test
+    void keywordReader_shouldReturn_keywords() throws Exception {
+        keywordReader.open(new ExecutionContext());
+
+        Keyword keyword = keywordReader.read();
+
+        Assertions.assertThat(keyword).isNotNull();
+    }
+}
