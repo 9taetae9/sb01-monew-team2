ed7d5c76c523fa7e6b6329a31cd8db2b4b76ff50
Kiki1875b <kevinheo0413@gmail.com>
2025-04-20 22:48:37 +0900

    feat: added InterestKeywordRepository #43

--- monew/src/main/java/com/codeit/team2/monew/module/domain/article/batch/BatchArticleWriter.java
+++ monew/src/main/java/com/codeit/team2/monew/module/domain/article/batch/BatchArticleWriter.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-// TODO : ARTICLE_INTEREST 에도 INSERT? 
+// TODO : ARTICLE_INTEREST 에도 INSERT?
 public class BatchArticleWriter implements ItemWriter<List<Article>> {
 
     private final ArticleRepository articleRepository;
