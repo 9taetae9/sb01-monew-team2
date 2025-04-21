6e6dfd2e63f3bf5df8993c097ac6b03d0226e95e
Soyoung Lee <soo7132@naver.com>
2025-04-21 14:35:57 +0900

    fix: 오타 수정 #23

--- monew/src/main/java/com/codeit/team2/monew/module/domain/subscription/repository/SubscriptionRepository.java
+++ monew/src/main/java/com/codeit/team2/monew/module/domain/subscription/repository/SubscriptionRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SubscriptionRepository extends JpaRepository<SubscriptionRepository, UUID> {
+public interface SubscriptionRepository extends JpaRepository<Subscription, UUID> {
 
     List<Subscription> findAllByInterest(Interest interest);
 
