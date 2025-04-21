9e8ed052ba50e795d09abf5810825b0868d19d91
TaeHyoun Kim <kth9903@gmail.com>
2025-04-21 15:01:11 +0900

    feat: 댓글 좋아요 엔티티 존재 여부, 조회 쿼리 메서드 추가

---
+++ monew/src/main/java/com/codeit/team2/monew/module/domain/comment/repository/CommentLikeRepository.java
@@ -0,0 +1,13 @@
+package com.codeit.team2.monew.module.domain.comment.repository;
+
+import com.codeit.team2.monew.module.domain.comment.entity.CommentLike;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentLikeRepository extends JpaRepository<CommentLike, UUID> {
+
+    boolean existsByCommentIdAndUserId(UUID commentId, UUID userId);
+
+    Optional<CommentLike> findByCommentIdAndUserId(UUID commentId, UUID userId);
+}
