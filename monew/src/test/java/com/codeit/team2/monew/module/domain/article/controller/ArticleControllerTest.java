4a793c42856835dd6548dfdf83656b0c545e2ad1
Kiki1875b <kevinheo0413@gmail.com>
2025-04-21 15:40:59 +0900

    test: created controller test for ArticleController (Red Cycle of TDD) #49

---
+++ monew/src/test/java/com/codeit/team2/monew/module/domain/article/controller/ArticleControllerTest.java
@@ -0,0 +1,64 @@
+package com.codeit.team2.monew.module.domain.article.controller;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.codeit.team2.monew.module.domain.article.dto.ArticleViewDto;
+import com.codeit.team2.monew.module.domain.article.service.ArticleService;
+import java.time.Instant;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(ArticleController.class)
+public class ArticleControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private ArticleService articleService;
+
+    @Test
+    void shouldReturnArticleViewDto_whenNewUserViews_Article() throws Exception {
+        // given
+        UUID userId = UUID.randomUUID();
+        UUID articleId = UUID.randomUUID();
+
+        ArticleViewDto dto = new ArticleViewDto(
+            UUID.randomUUID(),
+            userId,
+            Instant.now(),
+            articleId,
+            "NAVER",
+            "http://test.com",
+            "test article",
+            Instant.now(),
+            "test summary",
+            0,
+            1
+        );
+
+        BDDMockito.given(articleService.createUserArticleView(userId, articleId))
+            .willReturn(dto);
+
+        // when && then
+        mockMvc.perform(post("/api/articles/{articleId}/article-views", articleId)
+                .header("MoNew-Request-User-ID", userId.toString())
+                .contentType(MediaType.APPLICATION_JSON)
+            ).andExpect(status().isOk())
+            .andExpect(jsonPath("$.articleId").value(articleId.toString()))
+            .andExpect(jsonPath("$.viewedBy").value(userId.toString()))
+            .andExpect(jsonPath("$.articleTitle").value("뉴스제목"))
+            .andExpect(jsonPath("$.articleCommentCount").value(0))
+            .andExpect(jsonPath("$.articleViewCount").value(1));
+    }
+
+}
