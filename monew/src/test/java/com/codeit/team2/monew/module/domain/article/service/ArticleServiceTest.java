7825b59613f8bbc7953eb8c4fb13e139cf464a85
Kiki1875b <kevinheo0413@gmail.com>
2025-04-21 15:11:38 +0900

    test: added test to verify that article's view count does not increase when ArticleView already exists #49

--- monew/src/test/java/com/codeit/team2/monew/module/domain/article/service/ArticleServiceTest.java
+++ monew/src/test/java/com/codeit/team2/monew/module/domain/article/service/ArticleServiceTest.java
@@ -1,6 +1,5 @@
 package com.codeit.team2.monew.module.domain.article.service;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
@@ -61,20 +60,6 @@ public class ArticleServiceTest {
         User user = new User("test@gmail.com", "testUser", "test", false);
         ReflectionTestUtils.setField(user, "id", randomUserId);
 
-        ArticleViewDto returnDto = new ArticleViewDto(
-            UUID.randomUUID(),
-            randomUserId,
-            Instant.now(),
-            article.getId(),
-            article.getSource(),
-            article.getSourceUrl(),
-            article.getTitle(),
-            article.getPublishedDate(),
-            article.getSummary(),
-            0,
-            1
-        );
-
         ArticleView articleView = new ArticleView(user, article, Instant.now());
 
         BDDMockito.given(articleRepository.findById(article.getId()))
@@ -93,6 +78,37 @@ public class ArticleServiceTest {
         // then
         assertThat(response).isNotNull();
         assertThat(response.articleId()).isEqualTo(article.getId());
+        assertThat(response.articleViewCount()).isEqualTo(1);
+    }
+
+    @Test
+    void test_viewCount_shouldNotIncrease_OnDuplicateArticleView() {
+        // given
+        ArticleInterest interest = mock(ArticleInterest.class);
+        Article article = new Article(
+            "test", "NAVER", "https://test.com", "this is test summary", Set.of(interest), 1,
+            Instant.now(), false
+        );
+
+        ReflectionTestUtils.setField(article, "id", UUID.randomUUID());
+        UUID randomUserId = UUID.randomUUID();
+        User user = new User("test@gmail.com", "testUser", "test", false);
+        ReflectionTestUtils.setField(user, "id", randomUserId);
+
+        ArticleView articleView = new ArticleView(user, article, Instant.now());
+
+        BDDMockito.given(articleRepository.findById(article.getId()))
+            .willReturn(Optional.of(article));
+        BDDMockito.given(userRepository.findById(randomUserId))
+            .willReturn(Optional.of(user));
+        BDDMockito.given(articleViewRepository.findByUserAndArticle(any(), any()))
+            .willReturn(Optional.of(articleView));
+
+        // when
+        ArticleViewDto response = articleService.createUserArticleView(randomUserId,
+            article.getId());
+
+        // then
         assertThat(response.articleViewCount()).isEqualTo(1);
     }
 
