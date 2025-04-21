aa379af7cc864493d411c9a757d29833e6f1cfbc
Kiki1875b <kevinheo0413@gmail.com>
2025-04-21 15:06:38 +0900

    feat: ArticleMapper now converts (Article, ArticleView, UserId, CommentCount) into ArticleViewDto #49

--- monew/src/main/java/com/codeit/team2/monew/module/domain/article/mapper/ArticleMapper.java
+++ monew/src/main/java/com/codeit/team2/monew/module/domain/article/mapper/ArticleMapper.java
@@ -1,11 +1,14 @@
 package com.codeit.team2.monew.module.domain.article.mapper;
 
 
+import com.codeit.team2.monew.module.domain.article.dto.ArticleViewDto;
 import com.codeit.team2.monew.module.domain.article.dto.NaverArticleItemDto;
 import com.codeit.team2.monew.module.domain.article.entity.Article;
+import com.codeit.team2.monew.module.domain.article.entity.ArticleView;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.UUID;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -21,4 +24,19 @@ public interface ArticleMapper {
     Article naverArticleItemToEntity(NaverArticleItemDto dto);
 
     List<Article> naverArticleListToEntity(List<NaverArticleItemDto> dtoList);
+
+
+    @Mapping(target = "id", source = "articleView.id")
+    @Mapping(target = "viewedBy", source = "userId")
+    @Mapping(target = "createdAt", source = "articleView.createdAt")
+    @Mapping(target = "articleId", source = "article.id")
+    @Mapping(target = "source", source = "article.source")
+    @Mapping(target = "sourceUrl", source = "article.sourceUrl")
+    @Mapping(target = "articleTitle", source = "article.title")
+    @Mapping(target = "articlePublishedDate", source = "article.publishedDate")
+    @Mapping(target = "articleSummary", source = "article.summary")
+    @Mapping(target = "articleCommentCount", source = "commentCount")
+    @Mapping(target = "articleViewCount", source = "article.viewCount")
+    ArticleViewDto toResponseDto(Article article, ArticleView articleView, UUID userId,
+        int commentCount);
 }
