8b744aaff888d81ac626d955a9b156b2d4ccd2ce
Kiki1875b <kevinheo0413@gmail.com>
2025-04-21 15:08:09 +0900

    feat: added ArticleViewRepository and implemented ArticleServiceImpl (Green Cycle of TDD) #49

---
+++ monew/src/main/java/com/codeit/team2/monew/module/domain/article/service/ArticleService.java
@@ -0,0 +1,9 @@
+package com.codeit.team2.monew.module.domain.article.service;
+
+import com.codeit.team2.monew.module.domain.article.dto.ArticleViewDto;
+import java.util.UUID;
+
+public interface ArticleService {
+
+    ArticleViewDto createUserArticleView(UUID userId, UUID articleId);
+}
---
+++ monew/src/main/java/com/codeit/team2/monew/module/domain/article/service/ArticleServiceImpl.java
@@ -0,0 +1,50 @@
+package com.codeit.team2.monew.module.domain.article.service;
+
+import com.codeit.team2.monew.module.domain.article.dto.ArticleViewDto;
+import com.codeit.team2.monew.module.domain.article.entity.Article;
+import com.codeit.team2.monew.module.domain.article.entity.ArticleView;
+import com.codeit.team2.monew.module.domain.article.mapper.ArticleMapper;
+import com.codeit.team2.monew.module.domain.article.repository.ArticleRepository;
+import com.codeit.team2.monew.module.domain.article.repository.ArticleViewRepository;
+import com.codeit.team2.monew.module.domain.user.entity.User;
+import com.codeit.team2.monew.module.domain.user.repository.UserRepository;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ArticleServiceImpl implements ArticleService {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleViewRepository articleViewRepository;
+    private final UserRepository userRepository;
+    private final ArticleMapper articleMapper;
+
+    @Override
+    public ArticleViewDto createUserArticleView(UUID userId, UUID articleId) {
+
+        Article article = articleRepository.findById(articleId).orElseThrow(() -> {
+            log.debug("Article Not Found: id={}", articleId);
+            return new IllegalArgumentException();
+        });
+
+        User user = userRepository.findById(userId).orElseThrow(() -> {
+            log.debug("User Not Found: id={}", userId);
+            return new IllegalArgumentException();
+        });
+
+        ArticleView articleView = articleViewRepository.findByUserAndArticle(user, article)
+            .orElseGet(() -> {
+                article.incrementView();
+                return articleViewRepository.save(new ArticleView(user, article, Instant.now()));
+            });
+
+        // TODO : CommentRepository 완성시 관련 Comment 조회 로직
+        return articleMapper.toResponseDto(article, articleView, userId, 0);
+    }
+}
