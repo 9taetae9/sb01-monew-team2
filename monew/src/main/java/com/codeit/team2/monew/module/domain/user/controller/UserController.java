ceb78dd8916784bc49b5837c27bcb13a15b90a5d
김주언 <wndjs803@naver.com>
2025-04-21 11:49:16 +0900

    feat: UserController softDeleteUser 구현 #47

--- monew/src/main/java/com/codeit/team2/monew/module/domain/user/controller/UserController.java
+++ monew/src/main/java/com/codeit/team2/monew/module/domain/user/controller/UserController.java
@@ -10,6 +10,7 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,5 +48,15 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK)
             .header("MoNew-Request-User-ID", userDto.id().toString())
             .body(userDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> softDeleteUser(
+        @RequestHeader("MoNew-Request-User-ID") UUID loginId,
+        @PathVariable(name = "id") UUID userId
+    ) {
+        userService.softDeleteUser(loginId, userId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+            .build();
     }
 }
