8359618dc3e2fc947322b2d0f7b0992b0643072d
TaeHyoun Kim <kth9903@gmail.com>
2025-04-21 15:36:27 +0900

    feat: CommentLikeServiceImpl 좋아요 기능 코드 및 예외, 로깅 추가

--- monew/src/main/java/com/codeit/team2/monew/module/domain/comment/service/CommentLikeServiceImpl.java
+++ monew/src/main/java/com/codeit/team2/monew/module/domain/comment/service/CommentLikeServiceImpl.java
@@ -1,12 +1,54 @@
 package com.codeit.team2.monew.module.domain.comment.service;
 
+import com.codeit.team2.monew.module.domain.comment.entity.Comment;
+import com.codeit.team2.monew.module.domain.comment.entity.CommentLike;
+import com.codeit.team2.monew.module.domain.comment.mapper.CommentMapper;
+import com.codeit.team2.monew.module.domain.comment.repository.CommentLikeRepository;
+import com.codeit.team2.monew.module.domain.comment.repository.CommentRepository;
+import com.codeit.team2.monew.module.domain.user.entity.User;
+import com.codeit.team2.monew.module.domain.user.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class CommentLikeServiceImpl implements CommentLikeService {
 
+    private final CommentMapper commentMapper;
+    private final CommentRepository commentRepository;
+    private final CommentLikeRepository commentLikeRepository;
+    private final UserRepository userRepository;
+
     @Override
-    public CommentService like(UUID commentId, UUID userId) {
-        return null;
+    public CommentLike like(UUID commentId, UUID userId) {
+        Comment comment = commentRepository.findById(commentId)
+            .orElseThrow(() -> {
+                log.debug("Comment Not Found: commentId={}", commentId);
+                return new EntityNotFoundException("Comment Not Found");
+            });
+
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> {
+                log.debug("User Not Found: userId={}", userId);
+                return new EntityNotFoundException("User Not Found");
+            });
+
+        if (commentLikeRepository.existsByCommentIdAndUserId(commentId, userId)) {
+            log.debug("Already liked this comment: commentId = {}, userId={}",
+                commentId, userId);
+            throw new IllegalStateException("Already liked this comment");
+        }
+
+        CommentLike commentLike = CommentLike.create(comment, user);
+        comment.incrementLikeCount();
+
+        return commentLikeRepository.save(commentLike);
     }
 
     @Override
