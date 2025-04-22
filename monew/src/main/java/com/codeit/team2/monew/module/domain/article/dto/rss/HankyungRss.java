53d7cff1cf1905ff60eb5ea3a3eec40fc22d3157
Kiki1875b <kevinheo0413@gmail.com>
2025-04-22 15:21:38 +0900

    feat: added DTO to parse HankyungNewsClient response into Java Object #64

--- monew/src/main/java/com/codeit/team2/monew/module/domain/article/dto/rss/HankyungRss.java
+++ monew/src/main/java/com/codeit/team2/monew/module/domain/article/dto/rss/HankyungRss.java
@@ -1,5 +1,6 @@
 package com.codeit.team2.monew.module.domain.article.dto.rss;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
@@ -7,7 +8,7 @@ import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JacksonXmlRootElement(localName = "rss")
 public class HankyungRss {
 
@@ -20,6 +21,7 @@ public class HankyungRss {
 
     @Getter
     @Setter
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Channel {
 
         @JacksonXmlElementWrapper(useWrapping = false)
@@ -29,6 +31,7 @@ public class HankyungRss {
 
     @Getter
     @Setter
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Item {
 
         private String title;
--- monew/src/main/java/com/codeit/team2/monew/module/domain/article/mapper/ArticleMapper.java
+++ monew/src/main/java/com/codeit/team2/monew/module/domain/article/mapper/ArticleMapper.java
@@ -3,6 +3,7 @@ package com.codeit.team2.monew.module.domain.article.mapper;
 
 import com.codeit.team2.monew.module.domain.article.dto.ArticleViewDto;
 import com.codeit.team2.monew.module.domain.article.dto.NaverArticleItemDto;
+import com.codeit.team2.monew.module.domain.article.dto.rss.HankyungRss;
 import com.codeit.team2.monew.module.domain.article.entity.Article;
 import com.codeit.team2.monew.module.domain.article.entity.ArticleView;
 import java.time.ZonedDateTime;
@@ -15,6 +16,7 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring", imports = {ZonedDateTime.class, DateTimeFormatter.class})
 public interface ArticleMapper {
 
+    @Mapping(target = "id", ignore = true)
     @Mapping(source = "link", target = "sourceUrl")
     @Mapping(source = "description", target = "summary")
     @Mapping(target = "viewCount", constant = "0")
@@ -23,8 +25,21 @@ public interface ArticleMapper {
     @Mapping(target = "source", constant = "NAVER")
     Article naverArticleItemToEntity(NaverArticleItemDto dto);
 
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "source", constant = "HANKYUNG")
+    @Mapping(target = "sourceUrl", source = "link")
+    @Mapping(target = "viewCount", constant = "0")
+    @Mapping(target = "summary", constant = "")
+    @Mapping(target = "publishedDate", expression = "java(ZonedDateTime.parse(item.getPubDate(), DateTimeFormatter.RFC_1123_DATE_TIME).toInstant())")
+    @Mapping(target = "articleInterests", ignore = true)
+    @Mapping(target = "deleted", constant = "false")
+    Article hankyungRssToEntity(HankyungRss.Item item);
+
+
     List<Article> naverArticleListToEntity(List<NaverArticleItemDto> dtoList);
 
+    List<Article> hankyungRssListToEntity(List<HankyungRss.Item> items);
 
     @Mapping(target = "id", source = "articleView.id")
     @Mapping(target = "viewedBy", source = "userId")
@@ -39,4 +54,6 @@ public interface ArticleMapper {
     @Mapping(target = "articleViewCount", source = "article.viewCount")
     ArticleViewDto toResponseDto(Article article, ArticleView articleView, UUID userId,
         int commentCount);
+
+
 }
