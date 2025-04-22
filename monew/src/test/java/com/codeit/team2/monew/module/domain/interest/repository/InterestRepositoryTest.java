37cf1f7c20637c3f1636e1a12b12d7bc72a1fb69
yerimYeon <99879845+yinneu@users.noreply.github.com>
2025-04-22 15:18:01 +0900

    chore: 불필요한 import 제거 설정

--- monew/src/test/java/com/codeit/team2/monew/module/domain/interest/repository/InterestRepositoryTest.java
+++ monew/src/test/java/com/codeit/team2/monew/module/domain/interest/repository/InterestRepositoryTest.java
@@ -4,12 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.codeit.team2.monew.config.JpaConfig;
 import com.codeit.team2.monew.module.domain.interest.entity.Interest;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
