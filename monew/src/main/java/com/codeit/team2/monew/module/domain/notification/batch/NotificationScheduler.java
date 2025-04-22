ee3bd4ef900428e6f1b8ceedc16cd6b3ddc28289
Soyoung Lee <soo7132@naver.com>
2025-04-22 10:38:24 +0900

    feat: 알림 삭제 배치 기능 #55

---
+++ monew/src/main/java/com/codeit/team2/monew/module/domain/notification/batch/NotificationScheduler.java
@@ -0,0 +1,28 @@
+package com.codeit.team2.monew.module.domain.notification.batch;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationScheduler {
+
+    private final JobLauncher jobLauncher;
+    private final Job deleteNotificationJob;
+
+    @Scheduled(cron = "0 0 3 * * *")    // 매일 오전 3시
+    public void runJob() throws Exception {
+        JobParameters parameters = new JobParametersBuilder()
+            .addLong("timestamp", System.currentTimeMillis())
+            .toJobParameters();
+        log.info("Batch Processing Start: Delete confirmed and old notifications");
+        jobLauncher.run(deleteNotificationJob, parameters);
+    }
+}
