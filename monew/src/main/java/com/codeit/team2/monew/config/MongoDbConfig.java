9949b3d6d3eab067b418e7397d3ace5bd82fdf06
김주언 <wndjs803@naver.com>
2025-04-23 10:19:09 +0900

    chore(db): mongodb 연동 #81

---
+++ monew/src/main/java/com/codeit/team2/monew/config/MongoDbConfig.java
@@ -0,0 +1,8 @@
+package com.codeit.team2.monew.config;
+
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@EnableMongoRepositories
+public class MongoDbConfig {
+
+}
