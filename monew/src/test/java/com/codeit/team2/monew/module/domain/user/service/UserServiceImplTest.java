1b55d84fb181a18a4a82ebe43dcc645740d78bcf
김주언 <wndjs803@naver.com>
2025-04-23 18:03:58 +0900

    refactor(test): UserServiceImplTest ApplicationEventPublisher 의존성 추가 #81

--- monew/src/test/java/com/codeit/team2/monew/module/domain/user/service/UserServiceImplTest.java
+++ monew/src/test/java/com/codeit/team2/monew/module/domain/user/service/UserServiceImplTest.java
@@ -24,6 +24,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
@@ -33,6 +34,9 @@ class UserServiceImplTest {
 
     @Spy
     private UserMapper userMapper = Mappers.getMapper(UserMapper.class);
+
+    @Spy
+    private ApplicationEventPublisher applicationEventPublisher;
 
     @InjectMocks
     private UserServiceImpl userService;
