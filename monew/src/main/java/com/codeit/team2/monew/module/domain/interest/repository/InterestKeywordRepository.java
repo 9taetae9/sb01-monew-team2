92598f8d08a9d305d6d5b2d19817b1fdadddd7a9
Kiki1875b <kevinheo0413@gmail.com>
2025-04-29 09:48:15 +0900

    refactor: InterestKeywordRepository.findAllByKeyword now uses fetch join #129

--- monew/src/main/java/com/codeit/team2/monew/module/domain/interest/repository/InterestKeywordRepository.java
+++ monew/src/main/java/com/codeit/team2/monew/module/domain/interest/repository/InterestKeywordRepository.java
@@ -3,14 +3,26 @@ package com.codeit.team2.monew.module.domain.interest.repository;
 import com.codeit.team2.monew.module.domain.interest.entity.InterestKeyword;
 import com.codeit.team2.monew.module.domain.interest.entity.Keyword;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface InterestKeywordRepository extends JpaRepository<InterestKeyword, UUID> {
 
+
+    @Query("""
+        SELECT ik
+        FROM InterestKeyword ik
+        JOIN FETCH ik.interest
+        WHERE ik.keyword = :keyword
+        """)
     List<InterestKeyword> findAllByKeyword(Keyword keyword);
 
     boolean existsByKeyword(Keyword keyword);
+
+    Optional<InterestKeyword> findTopByKeywordOrderByCreatedAtDesc(Keyword keyword);
+
 }
