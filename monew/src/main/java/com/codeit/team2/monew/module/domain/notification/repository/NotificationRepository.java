23c4c3a1fc0882c9ffb3c6d2871faff2e470ade0
Soyoung Lee <soo7132@naver.com>
2025-04-29 16:31:47 +0900

    refactor: 레포지토리 정리

--- monew/src/main/java/com/codeit/team2/monew/module/domain/notification/repository/NotificationRepository.java
+++ monew/src/main/java/com/codeit/team2/monew/module/domain/notification/repository/NotificationRepository.java
@@ -3,8 +3,6 @@ package com.codeit.team2.monew.module.domain.notification.repository;
 import com.codeit.team2.monew.module.domain.notification.entity.Notification;
 import java.time.Instant;
 import java.util.UUID;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -19,20 +17,6 @@ public interface NotificationRepository extends JpaRepository<Notification, UUID
     @Modifying
     @Query("DELETE FROM Notification n WHERE n.confirmed = true AND n.createdAt < :time")
     int deleteByConfirmedIsTrueAndCreatedAtBefore(@Param("time") Instant time);
-
-    @Query("SELECT n FROM Notification n "
-        + "WHERE n.user.id = :userId "
-        + "AND n.confirmed = false "
-        + "AND n.createdAt > :cursor "
-        + "ORDER BY n.createdAt ASC, n.id DESC ")
-    Page<Notification> findPageWithCursor(@Param("userId") UUID userId,
-        @Param("cursor") Instant cursor, Pageable pageable);
-
-    @Query("SELECT n FROM Notification n "
-        + "WHERE n.user.id = :userId "
-        + "AND n.confirmed = false "
-        + "ORDER BY n.createdAt ASC, n.id DESC ")
-    Page<Notification> findFirstPage(@Param("userId") UUID userId, Pageable pageable);
 
     @Query("SELECT COUNT(n) FROM Notification  n WHERE n.user.id"
         + "=:userId AND n.confirmed = false ")
