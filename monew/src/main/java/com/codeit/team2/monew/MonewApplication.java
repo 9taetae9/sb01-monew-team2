8ff83790b7cd332e32d18ed8b9307443e2ca7e03
Soyoung Lee <soo7132@naver.com>
2025-04-30 16:47:16 +0900

    refactor: Spring Scheduler config 추가

--- monew/src/main/java/com/codeit/team2/monew/MonewApplication.java
+++ monew/src/main/java/com/codeit/team2/monew/MonewApplication.java
@@ -2,10 +2,8 @@ package com.codeit.team2.monew;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class MonewApplication {
 
     public static void main(String[] args) {
