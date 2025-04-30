47655b196de73d3345c7b7d27d7885aa461afab9
TaeHyoun Kim <kth9903@gmail.com>
2025-04-30 15:43:57 +0900

    feat: 기사 도메인 예외 코드 및 클래스 추가

---
+++ monew/src/main/java/com/codeit/team2/monew/module/domain/article/code/ArticleErrorCode.java
@@ -0,0 +1,24 @@
+package com.codeit.team2.monew.module.domain.article.code;
+
+import com.codeit.team2.monew.module.common.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public enum ArticleErrorCode implements ErrorCode{
+    ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "기사 정보를 찾을 수 없습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return this.httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+
+}
---
+++ monew/src/main/java/com/codeit/team2/monew/module/domain/article/exception/ArticleNotFoundException.java
@@ -0,0 +1,12 @@
+package com.codeit.team2.monew.module.domain.article.exception;
+
+import com.codeit.team2.monew.module.common.exception.BaseException;
+import com.codeit.team2.monew.module.domain.article.code.ArticleErrorCode;
+import java.util.Map;
+import java.util.UUID;
+
+public class ArticleNotFoundException extends BaseException {
+    public ArticleNotFoundException(UUID articleId) {
+        super(ArticleErrorCode.ARTICLE_NOT_FOUND, Map.of("articleId", articleId));
+    }
+}
