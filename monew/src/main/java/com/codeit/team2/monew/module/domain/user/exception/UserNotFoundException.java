df7960f12584e991273a6abb0a90afdef79a186b
TaeHyoun Kim <kth9903@gmail.com>
2025-04-30 15:44:14 +0900

    feat: 사용자 도메인 예외 코드 및 클래스 추가

---
+++ monew/src/main/java/com/codeit/team2/monew/module/domain/user/exception/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package com.codeit.team2.monew.module.domain.user.exception;
+
+import com.codeit.team2.monew.module.common.exception.BaseException;
+import com.codeit.team2.monew.module.domain.user.code.UserErrorCode;
+import java.util.Map;
+import java.util.UUID;
+
+public class UserNotFoundException extends BaseException {
+    public UserNotFoundException(UUID userId) {
+        super(UserErrorCode.USER_NOT_FOUND, Map.of("userId", userId));
+    }
+}
