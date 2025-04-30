7908a665ac3744c3eca7f35b65635ac7520d8a8e
yerimYeon <99879845+yinneu@users.noreply.github.com>
2025-04-30 11:10:42 +0900

    test: 관심사 유사도 관련 테스트 disabled

--- monew/src/test/java/com/codeit/team2/monew/module/domain/interest/service/InterestNameSimilarityServiceTest.java
+++ monew/src/test/java/com/codeit/team2/monew/module/domain/interest/service/InterestNameSimilarityServiceTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.codeit.team2.monew.module.domain.interest.exception.InvalidSimilarityInputException;
 import org.apache.commons.text.similarity.LevenshteinDistance;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Tag;
@@ -13,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+@Disabled
 @SpringBootTest
 @Tag("integration")
 @ActiveProfiles({"test-temp"})
