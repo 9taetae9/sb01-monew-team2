5363665f16958084bf3a9b63d48bb1634dd0ed03
Soyoung Lee <soo7132@naver.com>
2025-05-02 16:13:21 +0900

    fix: 에러 코드 설명 수정 #173

--- monew/src/main/java/com/codeit/team2/monew/module/domain/notification/exception/NotificationErrorCode.java
+++ monew/src/main/java/com/codeit/team2/monew/module/domain/notification/exception/NotificationErrorCode.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 public enum NotificationErrorCode implements ErrorCode {
 
     NOTIFICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "알림 정보를 찾을 수 없습니다."),
-    NOTIFICATION_ACCESS_DENIED(HttpStatus.BAD_REQUEST, "접근 가능한 알림이 아닙니다.");
+    NOTIFICATION_ACCESS_DENIED(HttpStatus.BAD_REQUEST, "접근 불가능한 알림입니다.");
     private final HttpStatus status;
     private final String message;
 
