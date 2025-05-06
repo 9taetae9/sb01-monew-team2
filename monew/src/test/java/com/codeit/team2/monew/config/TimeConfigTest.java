95c1f60b82cb4c1daa41ee733f6e499bf5e3fdd3
TaeHyoun Kim <kth9903@gmail.com>
2025-05-06 14:02:00 +0900

    test: timeConfig 테스트 코드 작성

---
+++ monew/src/test/java/com/codeit/team2/monew/config/TimeConfigTest.java
@@ -0,0 +1,29 @@
+package com.codeit.team2.monew.config;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.ZoneId;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(properties = "app.timezone=Asia/Seoul")
+class TimeConfigTest {
+
+    @Autowired
+    private ZoneId zoneId;
+
+    @Autowired
+    private String timezoneId;
+
+    @Test
+    void testTimeConfigBeans() {
+        assertNotNull(zoneId);
+        assertEquals(ZoneId.of("Asia/Seoul"), zoneId);
+
+        assertNotNull(timezoneId);
+        assertEquals("Asia/Seoul", timezoneId);
+    }
+}
