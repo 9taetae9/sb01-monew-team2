163da69b85b77bc60de3843339287b2f419c6097
김주언 <wndjs803@naver.com>
2025-05-07 11:20:56 +0900

    test: 사용자 수정, 로그인 성공 #177

--- monew/src/test/java/com/codeit/team2/monew/module/domain/user/controller/UserControllerTest.java
+++ monew/src/test/java/com/codeit/team2/monew/module/domain/user/controller/UserControllerTest.java
@@ -1,11 +1,15 @@
 package com.codeit.team2.monew.module.domain.user.controller;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.codeit.team2.monew.module.domain.user.dto.request.UserLoginRequest;
 import com.codeit.team2.monew.module.domain.user.dto.request.UserRegisterRequest;
+import com.codeit.team2.monew.module.domain.user.dto.request.UserUpdateRequest;
 import com.codeit.team2.monew.module.domain.user.dto.response.UserDto;
 import com.codeit.team2.monew.module.domain.user.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -51,6 +55,61 @@ class UserControllerTest {
                     .content(objectMapper.writeValueAsString(userRegisterRequest))
             )
             .andExpect(status().isCreated())
+            .andExpect(jsonPath("$.id").value(userId.toString()))
+            .andExpect(jsonPath("$.email").value(email))
+            .andExpect(jsonPath("$.nickname").value(nickname))
+            .andExpect(jsonPath("$.createdAt").value(createdAt.toString()));
+    }
+
+    @Test
+    void 사용자_수정_성공() throws Exception {
+        // given
+        String email = "email@a.com";
+        String nickname = "newNickname";
+        UserUpdateRequest userUpdateRequest = new UserUpdateRequest(nickname);
+
+        UUID userId = UUID.randomUUID();
+        UUID loginId = userId;
+        Instant createdAt = Instant.now();
+        UserDto userDto = new UserDto(userId, email, nickname, createdAt);
+        when(userService.updateUser(loginId, userId, userUpdateRequest)).thenReturn(userDto);
+
+        // when & then
+        mockMvc.perform(
+                patch("/api/users/{userId}", userId)
+                    .header("Monew-Request-User-Id", loginId)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(userUpdateRequest))
+            )
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.id").value(userId.toString()))
+            .andExpect(jsonPath("$.email").value(email))
+            .andExpect(jsonPath("$.nickname").value(nickname))
+            .andExpect(jsonPath("$.createdAt").value(createdAt.toString()));
+    }
+
+    @Test
+    void 사용자_로그인_성공() throws Exception {
+        // given
+        String email = "email@a.com";
+        String nickname = "nickname1";
+        String password = "password";
+        UserLoginRequest userLoginRequest = new UserLoginRequest(email, password);
+
+        UUID userId = UUID.randomUUID();
+        UUID loginId = userId;
+        Instant createdAt = Instant.now();
+        UserDto userDto = new UserDto(userId, email, nickname, createdAt);
+        when(userService.login(userLoginRequest)).thenReturn(userDto);
+
+        // when & then
+        mockMvc.perform(
+                post("/api/users/login")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(userLoginRequest))
+            )
+            .andExpect(status().isOk())
+            .andExpect(header().string("Monew-Request-User-Id", userId.toString()))
             .andExpect(jsonPath("$.id").value(userId.toString()))
             .andExpect(jsonPath("$.email").value(email))
             .andExpect(jsonPath("$.nickname").value(nickname))
