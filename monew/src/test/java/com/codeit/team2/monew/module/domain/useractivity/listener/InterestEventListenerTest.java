5cb674ff3c9c92866238e494b1eb8e005a805d2d
김주언 <wndjs803@naver.com>
2025-05-07 14:46:03 +0900

    test: updateSubscriptionItem 호출 성공 #181

---
+++ monew/src/test/java/com/codeit/team2/monew/module/domain/useractivity/listener/InterestEventListenerTest.java
@@ -0,0 +1,41 @@
+package com.codeit.team2.monew.module.domain.useractivity.listener;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import com.codeit.team2.monew.module.domain.interest.entity.Interest;
+import com.codeit.team2.monew.module.domain.interest.event.InterestUpdateEvent;
+import com.codeit.team2.monew.module.domain.useractivity.service.MongoUserActivityService;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class InterestEventListenerTest {
+
+    @Mock
+    private MongoUserActivityService userActivityService;
+
+    @InjectMocks
+    private InterestEventListener listener;
+
+    @Test
+    void updateSubscriptionItem_호출_성공() {
+        // given
+        Interest interest = mock(Interest.class);
+        List<String> keywords = new ArrayList<>();
+        UUID userId = UUID.randomUUID();
+        InterestUpdateEvent event = new InterestUpdateEvent(interest, keywords, userId);
+
+        // when
+        listener.updateSubscriptionItem(event);
+
+        // then
+        verify(userActivityService).updateSubscriptionItemInActivity(interest, keywords, userId);
+    }
+}
