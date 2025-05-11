724848266803151831c55378f5e4ef90df5b2554
Soyoung Lee <soo7132@naver.com>
2025-05-11 18:35:16 +0900

    fix: CommnetLikeService에서 DTO 반환 #259

--- monew/src/main/java/com/codeit/team2/monew/module/domain/comment/service/CommentLikeService.java
+++ monew/src/main/java/com/codeit/team2/monew/module/domain/comment/service/CommentLikeService.java
@@ -1,11 +1,11 @@
 package com.codeit.team2.monew.module.domain.comment.service;
 
-import com.codeit.team2.monew.module.domain.comment.entity.CommentLike;
+import com.codeit.team2.monew.module.domain.comment.dto.CommentLikeDto;
 import java.util.UUID;
 
 public interface CommentLikeService {
 
-    CommentLike like(UUID commentId, UUID userId);
+    CommentLikeDto like(UUID commentId, UUID userId);
 
     void unlike(UUID commentId, UUID userId);
 
